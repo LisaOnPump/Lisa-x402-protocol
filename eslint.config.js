6d86900cfef7a6fc53483024ce9729c557daba13
RISA <bhattharssh@gmail.com>
2025-10-28 16:13:19 +1030

    Enhance ESLint config for TypeScript support
    Added TypeScript support to ESLint configuration.

--- eslint.config.js
+++ eslint.config.js
@@ -18,3 +18,25 @@ export default [
   },
   js.configs.recommended,
 ]
+
+import js from '@eslint/js'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      sourceType: 'module',
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+    },
+  },
+]
+
